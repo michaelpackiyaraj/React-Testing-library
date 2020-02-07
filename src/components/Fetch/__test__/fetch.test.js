7181acb5144d7e80b2bf9ec67f22d714841590ac
Michael Packiyaraj <micpacki@publicisgroupe.net>
2020-02-08 03:26:48 +0530

    Fetch component added

---
+++ src/components/Fetch/__test__/fetch.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, waitForElement } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
+import axiosMock from "./../__mocks__/axios";
+import Fetch from './../index';
+
+describe("Testing a mock Axios request", () => {
+    it("fetch and display data", async ()=>{
+        axiosMock.get.mockResolvedValueOnce({data:{ greetings:'Hello there'}});
+
+        const url = '/greetings';
+        const {getByTestId} = render(<Fetch url = {url} />);
+        expect(getByTestId('loading')).toHaveTextContent('Loading data...');
+
+        const resolvedSpan = await waitForElement(()=>{ 
+           return getByTestId("resolved")
+        });
+        expect(resolvedSpan).toHaveTextContent('Hello there');
+        //expect(resolvedSpan).toHaveBeenCalledTimes(1);
+        //expect(resolvedSpan).toHaveBeenCalledWith(url);
+        //https://www.youtube.com/watch?v=Ngj2f1n9pUw
+    })
+});
